3e3717e84e85e0723802de8db5ffcd0436e28384
Jiahui Feng <feng_jeffrey@yahoo.com>
2022-06-01 01:47:18 -0400

    Functionality complete need testing still

--- src/Pizza.js
+++ src/Pizza.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import * as yup from 'yup';
 import formSchema from "./validation/formSchema.js";
+import axios from "axios";
 
 export default function Pizza() {
 
@@ -15,9 +16,13 @@ export default function Pizza() {
         special: '',
     }
 
+    const initialOrders = [];
+
     //STATES
     const [formValues, setFormValues] = useState(initialFormValues);
     const [formErrors, setFormErrors] = useState('');
+    const [orders, setOrders] = useState(initialOrders);
+    const [disabled, setDisabled] = useState(true);
 
 
     //HELPERS
@@ -28,42 +33,86 @@ export default function Pizza() {
             .catch(() => setFormErrors('name must be at least 2 characters'))
     }
 
-    const submit = () => {
-        
+    const inputChange = e => {
+        const {name, value, checked, type } = e.target;
+        const valueToUse = type === "checkbox" ? checked : value;
+        validate(name, valueToUse);
+        setFormValues({...formValues, [name]: valueToUse});
     }
 
+    const submit = (e) => {
+
+        e.preventDefault();
+
+        const newOrder = {
+            name: formValues.name.trim(),
+            size: formValues.size,
+            special: formValues.special.trim(),
+            toppings: ["pepperoni", "mushroom", "bacon", "sausage"].filter(top => formValues[top])
+        }
+
+        axios.post("https://reqres.in/api/orders", newOrder)
+            .then(res => {
+                setOrders([...orders, res.data]);
+            })
+            .catch(err => console.error(err))
+            .finally(() => setFormValues(initialFormValues))
+        }
+    
+
+    //SIDE EFFECTS
+
+    useEffect(() => {
+        formSchema.isValid(formValues).then(valid => setDisabled(!valid))
+    }, [formValues]);
 
     return(
         <div>
-            <form id="pizza-form">
+            <div>
+                <h1>{formErrors}</h1>
+            </div>
+            <form id="pizza-form" onSubmit={submit}>
                 <label>
                     Name:
-                    <input type="text" name="name" id="name-input" />
+                    <input type="text" name="name" id="name-input" value={formValues.name} onChange={inputChange}/>
                 </label>
 
                 <label>
                     Size:
-                    <select id="size-dropdown" name="size">
+                    <select id="size-dropdown" name="size" value={formValues.size} onChange={inputChange}>
+                        <option value=''>Select a Size</option>
                         <option value="small">Small</option>
                         <option value="medium">Medium</option>
                         <option value="large">Large</option>
                     </select>
                 </label>
-
-                <label>
-                    Toppings:
-                    <input type="checkbox" name="pepperoni">Pepperoni</input>
-                    <input type="checkbox" name="mushroom">Mushroom</input>
-                    <input type="checkbox" name="bacon">Bacon</input>
-                    <input type="checkbox" name="sausage">Sausage</input>
-                </label>
+                <div> Toppings: <br />
+                    <label>
+                        Pepperoni
+                        <input type="checkbox" name="pepperoni" onChange={inputChange} checked={formValues.pepperoni}/>
+                    </label>
+                    <label>
+                        Mushroom
+                        <input type="checkbox" name="mushroom" onChange={inputChange}checked={formValues.mushroom}/>
+                    </label>
+                    <label>
+                        Bacon
+                        <input type="checkbox" name="bacon" onChange={inputChange} checked={formValues.bacon}/>
+                    </label>
+                    <label>
+                        Sausage
+                        <input type="checkbox" name="sausage" onChange={inputChange} checked={formValues.sausage}/>
+                    </label>
+                </div>
 
                 <label>
                     Special Instructions:
-                    <input type="text" name="special" id="special-text" />
+                    <input type="text" name="special" id="special-text" value={formValues.special} onChange={inputChange}/>
                 </label>
 
-                <button id="order-button" onsubmit={submit}>Order</button>
+                <br />
+
+                <button disabled={disabled} id="order-button">Order</button>
             </form>
         </div>
     )
--- src/validation/formSchema.js
+++ src/validation/formSchema.js
@@ -1,9 +1,20 @@
 import * as yup from 'yup';
 
-const formSchema = yup.object.shape({
+const formSchema = yup.object().shape({
     name: yup
         .string()
         .trim()
         .required()
-        .min(2, "name must be at least 2 characters")
-})
+        .min(2, "name must be at least 2 characters"),
+    size: yup
+        .string()
+        .oneOf(["small", "medium", "large"]),
+    special: yup
+        .string(),
+    pepperoni: yup.boolean(),
+    mushroom: yup.boolean(),
+    bacon: yup.boolean(),
+    sausage: yup.boolean()
+});
+
+export default formSchema;
